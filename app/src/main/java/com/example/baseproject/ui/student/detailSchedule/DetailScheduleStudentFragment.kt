0a093351d974a1f2b3e4ca60086f28d108b0024b
Phạm Văn Khải <Pham Van Khai>
2023-10-01 23:32:36 +0700

    Test

--- app/src/main/java/com/example/baseproject/ui/student/detailSchedule/DetailScheduleStudentFragment.kt
+++ app/src/main/java/com/example/baseproject/ui/student/detailSchedule/DetailScheduleStudentFragment.kt
@@ -1,26 +1,18 @@
 package com.example.baseproject.ui.student.detailSchedule
 
 import android.os.Bundle
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.baseproject.R
 import com.example.baseproject.databinding.FragmentDetailScheduleStudentBinding
-import com.example.baseproject.databinding.FragmentScheduleStudentBinding
-import com.example.baseproject.databinding.FragmentScheduleTeacherBinding
-import com.example.baseproject.model.Course
 import com.example.baseproject.navigation.AppNavigation
-import com.example.baseproject.ui.student.schedule.adapter.CourseStudentRegisterAdapter
-import com.example.baseproject.ui.teacher.schedule.adapter.CourseTeacher
 import com.example.baseproject.util.BundleKey
 import com.example.core.base.fragment.BaseFragment
 import com.example.core.pref.RxPreferences
 import com.example.core.utils.collectFlowOnView
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
-import java.util.Locale
 import javax.inject.Inject
 
 @AndroidEntryPoint
